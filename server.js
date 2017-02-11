93cf0dd30d62caef2483550d0ddec8beccdaf95f
Paul H Salmon <Pauly@MacBook-Pro.local>
2017-02-11 21:52:01 +1000

    sanitsation

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var express = require('express');
-var app = express();
-var PORT = process.env.PORT || 3000;
-var todoNextId = 1;
 var bodyParser = require('body-parser');
+var _ = require('underscore')
+
+var PORT = process.env.PORT || 3000;
+var app = express();
 var todos = [];
 var nextTodoId = 1;
 
 app.use(bodyParser.json());
-
 
 
 app.get('/todos', function(req, res){
@@ -15,14 +15,8 @@ app.get('/todos', function(req, res){
 })
 
 app.get('/todos/:id', function(req, res){
-  var todoId = parseInt(req.params.id)
-  var matchedTodo;
-
-  todos.forEach(function(todo){
-    if(todoId === todo.id ){
-      matchedTodo = todo;
-    }
-  });
+  var todoId = parseInt(req.params.id, 10)
+  var matchedTodo =_.findWhere(todos, {id:todoId})
 
   if(matchedTodo){
     res.json(matchedTodo)
@@ -33,14 +27,21 @@ app.get('/todos/:id', function(req, res){
 })
 
 app.get('/', function(req, res){
-  res.send('Todo api Root')
+  res.send('Todo API root')
 })
 
 app.post('/todos', function(req, res){
-  var body = req.body;
+  //sanitises the data. using underscore. to select only the inputs to the database that you want
+  //this is underscore. its filters the request body, and only selects the value properites associated with the two set . description, completed
+	var body = _.pick(req.body, 'description', 'completed');
+
+  if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+  return res.status(400).send();
+  }
+  body.description = body.description.trim();
   body.id = nextTodoId++;
 
-   console.log(body)
+  console.log(body)
 
   todos.push(body)
   console.log('description: ' + body.description)
